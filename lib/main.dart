cb2e419a2bd544db664b1aa99a2f5746ef86f3e9
adavidson-smt <alec.davidson@smashmytrash.com>
2021-04-19 22:42:15 -0400

    Finally working

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,362 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_calc/styling.dart';
 import 'package:flutter_calc/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Quicksand'),
-      home: Calculator(),
-    ),
-  );
+  runApp(CalculatorApp());
 }
 
-class Calculator extends StatelessWidget {
+class CalculatorApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryLightColor, // primaryColor imports from data.dart
-      body: Column(
-        children: <Widget>[
-          NavigationBar(),
-          InputHistory(),
-          InputBar(),
-          NumPad(),
-        ],
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Quicksand'),
+      home: Calculator(),
     );
   }
 }
 
-class _Calculator extends StatefulWidget {
+class Calculator extends StatefulWidget {
+  Calculator({Key key}) : super(key: key);
+
   @override
   _CalculatorState createState() => _CalculatorState();
 }
 
-class _CalculatorState extends State<_Calculator> {
+class _CalculatorState extends State<Calculator> {
+  String _inputBar = '';
+  String _inputOperator = '';
+
+  void _addInput(num) {
+    setState(() {
+      _inputBar += num;
+    });
+  }
+
+  void _clearInput() {
+    setState(() {
+      _inputBar = '';
+    });
+  }
+
+  void _add() {
+    setState(() {
+      double num1 = double.parse(_inputBar);
+      _inputBar += ' + ';
+    });
+  }
+
+    void _equals() {
+    setState(() {
+      double num1 = double.parse(_inputBar);
+      _inputBar += ' + ';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryLightColor, // primaryColor imports from data.dart
-      body: Column(
-        children: <Widget>[
-        ],
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            // NavigationBar
+            Container(
+              height: 50,
+              child: Padding(
+                padding: EdgeInsets.only(left: 25, right: 25, top: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    NavBarItem(icon: CupertinoIcons.clock),
+                    Spacer(),
+                    NavBarItem(icon: CupertinoIcons.back),
+                  ],
+                ),
+              ),
+            ),
+            // NavigationBar End
+            // -----------------
+            // InputHistory
+            Container(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 70, top: 100),
+                child: Text(
+                  'input history',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+            // InputHistory End
+            // -----------------
+            // InputBar
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 60, top: 15),
+                    child: SingleChildScrollView(
+                      child: Text(
+                        '$_inputBar',
+                        style: TextStyle(
+                            fontSize: 50, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // InputBar End
+            // -----------------
+            // NumPad
+            Container(
+              child: Padding(
+                padding: EdgeInsets.only(top: 60),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          TextButton(
+                            style: TextButton.styleFrom(primary: lightText),
+                            onPressed: () {
+                              _clearInput();
+                            },
+                            child: Text(
+                              'C',
+                              style: TextStyle(
+                                  fontSize: 40, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(primary: lightText),
+                            onPressed: () {},
+                            child: Text(
+                              '+/-',
+                              style: TextStyle(
+                                  fontSize: 40, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(primary: lightText),
+                            onPressed: () {},
+                            child: Text(
+                              '%',
+                              style: TextStyle(
+                                  fontSize: 40, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(primary: lightText),
+                            onPressed: () {},
+                            child: Text(
+                              '/',
+                              style: TextStyle(
+                                  fontSize: 40, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('7');
+                          },
+                          child: Text(
+                            '7',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('8');
+                          },
+                          child: Text(
+                            '8',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('9');
+                          },
+                          child: Text(
+                            '9',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {},
+                          child: Text(
+                            'x',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('4');
+                          },
+                          child: Text(
+                            '4',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('5');
+                          },
+                          child: Text(
+                            '5',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('6');
+                          },
+                          child: Text(
+                            '6',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {},
+                          child: Text(
+                            '-',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('1');
+                          },
+                          child: Text(
+                            '1',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('2');
+                          },
+                          child: Text(
+                            '2',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('3');
+                          },
+                          child: Text(
+                            '3',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {},
+                          child: Text(
+                            '+',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {},
+                          child: Text(
+                            '',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('0');
+                          },
+                          child: Text(
+                            '0',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {
+                            _addInput('.');
+                          },
+                          child: Text(
+                            '.',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(primary: lightText),
+                          onPressed: () {},
+                          child: Text(
+                            '=',
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // NumPad End
+          ],
+        ),
       ),
     );
   }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -3,37 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calc/styling.dart';
 import 'package:flutter_calc/math.dart';
 
-String _inputBar = '';
-
-class NavigationBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      child: Padding(
-        padding: EdgeInsets.only(left: 25, right: 25, top: 50),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            NavBarItem(icon: CupertinoIcons.clock),
-            Spacer(),
-            NavBarItem(icon: CupertinoIcons.back),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NavBarItem extends StatefulWidget {
-  final IconData icon;
+class NavBarItem extends StatelessWidget {
   const NavBarItem({Key key, this.icon}) : super(key: key);
-
-  @override
-  _NavBarItemState createState() => _NavBarItemState();
-}
-
-class _NavBarItemState extends State<NavBarItem> {
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -45,7 +17,7 @@ class _NavBarItemState extends State<NavBarItem> {
       child: IconButton(
         iconSize: 30,
         icon: Icon(
-          widget.icon,
+          icon,
           color: lightText,
         ),
         onPressed: () {},
@@ -54,164 +26,7 @@ class _NavBarItemState extends State<NavBarItem> {
   }
 }
 
-class InputHistory extends StatefulWidget {
-  final String inputHistory;
-  InputHistory({Key key, this.inputHistory}) : super(key: key);
 
-  @override
-  _InputHistoryState createState() => _InputHistoryState();
-}
-
-class _InputHistoryState extends State<InputHistory> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.bottomRight,
-      child: Padding(
-        padding: EdgeInsets.only(right: 70, top: 100),
-        child: Text(
-          'input history',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-        ),
-      ),
-    );
-  }
-}
-
-class InputBar extends StatefulWidget {
-  InputBar({Key key}) : super(key: key);
-
-  @override
-  _InputBarState createState() => _InputBarState();
-}
-
-class _InputBarState extends State<InputBar> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(right: 60, top: 15),
-          child: Text(
-            '$_inputBar',
-            style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
-          ),
-        ),
-      ],
-    ));
-  }
-}
-
-
-class NumPad extends StatelessWidget {
-  const NumPad({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.only(top: 60),
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  NumButton(text: 'C'),
-                  NumButton(text: '+/-'),
-                  NumButton(text: '%'),
-                  NumButton(text: '/'),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                NumButton(text: '7'),
-                NumButton(text: '8'),
-                NumButton(text: '9'),
-                NumButton(text: 'X'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                NumButton(text: '4'),
-                NumButton(text: '5'),
-                NumButton(text: '6'),
-                NumButton(text: '-'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                NumButton(text: '1'),
-                NumButton(text: '2'),
-                NumButton(text: '3'),
-                AddButton(text: '+'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                NumButton(text: ''),
-                NumButton(text: '0'),
-                NumButton(text: '.'),
-                NumButton(text: '='),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NumButton extends StatefulWidget {
-  const NumButton({Key key, this.text}) : super(key: key);
-
-  final String text;
-  @override
-  _NumButtonState createState() => _NumButtonState();
-}
-
-class _NumButtonState extends State<NumButton> {
-  void _addInput() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _inputBar += '1';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        // height: 65,
-        // width: Max,
-        decoration: BoxDecoration(
-          color: primaryLightColor,
-        ),
-        child: TextButton(
-          style: TextButton.styleFrom(primary: lightText),
-          onPressed: () {
-            _addInput();
-          },
-          child: Text(
-            widget.text,
-            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 class AddButton extends StatelessWidget {
   final String text;
@@ -227,9 +42,7 @@ class AddButton extends StatelessWidget {
       ),
       child: TextButton(
         style: TextButton.styleFrom(primary: lightText),
-        onPressed: () {
-          _inputBar += '1';
-        },
+        onPressed: () {},
         child: Text(
           text,
           style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
